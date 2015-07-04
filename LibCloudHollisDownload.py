7839218b74abf06a15508f6867b8f47cde474ead
comstock <comstock@users.noreply.github.com>
2015-07-04 10:31:48 -0400

    modified to generate 1  xml file per downloaded catalog record
    Each record called back from LibraryCloud is saved as a single file that includes the catalog ID in the filename.

--- LibCloudHollisDownload.py
+++ LibCloudHollisDownload.py
@@ -9,20 +9,19 @@ from StringIO import StringIO
 
 fileExtention = "xml"
 libcloud_call = ""
-#fileNameStem = "HYL_gazeteers"
-SingleID = ""
+SingleID = "XXX"
 libcloud_stem = "http://api.lib.harvard.edu/v2/items."
-hollisRecords = "/home/comstock/Documents/python/data/hollisRecords.xml"
+hollisRecordsStem = "/home/comstock/Documents/python/data/hollisRecords" + "_"
 
 with open("/home/comstock/Documents/python/data/hollisIDs.txt",'r') as hollisList:
     for line in hollisList:
-        # print line
-        singleID = line
-        libcloud_call = libcloud_stem + fileExtention + "?recordIdentifier=" + singleID
+        #print line
+        SingleID = line
+        libcloud_call = libcloud_stem + fileExtention + "?recordIdentifier=" + SingleID
         libcloud_call = libcloud_call.rstrip()
-
+        hollisRecordsFile = hollisRecordsStem + SingleID + "." + fileExtention
         #print libcloud_call
-        f = open (hollisRecords,'w')
+        f = open (hollisRecordsFile,'w')
         c = pycurl.Curl()
         c.setopt(c.URL, libcloud_call)
         c.setopt(c.WRITEDATA, f)
