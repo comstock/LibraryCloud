fb0f28a8b808c0c446fd4fb23b6f3d3906213c62
comstock <comstock@users.noreply.github.com>
2015-12-21 13:48:30 -0500

    Update dirImages_to_html.sh

--- dirImages_to_html.sh
+++ dirImages_to_html.sh
@@ -7,14 +7,14 @@
 # generate list of JPG files in current directory
 ls -1 *.jpg  > filelist.txt
 
-# If index.html exists, rename it so we can build a new one
-if [ -e ./index.html ]
+# If imageIndex.html exists, rename it so we can build a new one
+if [ -e ./imageIndex.html ]
 then
-mv index.html "index_"$SECONDS".html"
+mv imageIndex.html "index_"$SECONDS".html"
 fi
 
-# remove any existing index.html file
-# rm index.html
+# remove any existing imageIndex.html file
+# rm imageIndex.html
 
 echo "<?xml version="1.0" encoding="iso-8859-1"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -24,16 +24,21 @@ echo "<?xml version="1.0" encoding="iso-8859-1"?>
 		<title>BIll Rules!</title>
 </head>
 </body>
-" >> index.html
+" >> imageIndex.html
 
 # TEST #while read line; do echo "$line"; done < filelist.txt
 
 while read line; do echo "<a href=\"./$line.html\"><img src=\"$line\" alt=\"[$line]\" width=\"800\" /></a></br></br>
-" >> index.html; done < filelist.txt
+" >> imageIndex.html; done < filelist.txt
 
-echo "</body><hr /></html>" >> index.html
+echo "</body><hr /></html>" >> imageIndex.html
 
 ## Build individual web pages for individual images
 
-while read line; do echo "<html><head><title>$line</title></head><body><img src=\"$line\" /><hr/><a href=\"index.html\">Back</a></body></html>" > $line.html;done < filelist.txt
-
+while read line; do echo "
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>$line</title></head>
+<body><img src=\"$line\" /><hr/><a href=\"imageIndex.html\">Back</a></body></html>" > $line.html;done < filelist.txt
