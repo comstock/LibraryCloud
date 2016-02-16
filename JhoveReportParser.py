fd4491484ba2ea6007189673c3b10db67edd2208
comstock <comstock@users.noreply.github.com>
2016-02-16 15:32:32 -0500

    Update JhoveReportParser.py

--- JhoveReportParser.py
+++ JhoveReportParser.py
@@ -7,6 +7,9 @@
 # # Using the following command, a full JHOVE output report generated for each examined file, and all reports concatenated into a single text file
 # java -jar ./JhoveApp.jar -h text ~/anyDIR/CharlieHebdo/* > ./CharlieHebdoPictures_JHOVE_TXT_20160216.txt
 #
+# I eliminated LONG file paths by running the output through the following SED script
+# s/\/run\/user\/1000\/gvfs\/smb-share:server=pentos-smb.ad.dil.harvard.edu,share=jellyPlanet\/TEST\/COMSTOCK\/CharlieHebdo//g;
+#
 
 import os, sys
 import re
